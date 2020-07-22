b63579f887f2119587a00affa2c5fc4cea2ffdc2
sreeragpavithran <46649210+sreeragpavithran@users.noreply.github.com>
2020-07-22 10:54:13 +0530

    XHR GET

--- XMLHttpRequest/xhr.js
+++ XMLHttpRequest/xhr.js
@@ -2,7 +2,15 @@ const getBtn = document.getElementById('get-btn');
 const postBtn = document.getElementById('post-btn');
 
 const getData = ()=>{
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://reqres.in/api/users');
 
+    xhr.onload = () => {
+        let json = JSON.parse(xhr.response);
+        console.log(json)
+    }
+
+    xhr.send();     
 }
 
 const postData = ()=>{
