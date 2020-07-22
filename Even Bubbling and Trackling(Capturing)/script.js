834c34fa1eea4508e0d1e3d45d0c8dccad79b19e
sreeragpavithran <46649210+sreeragpavithran@users.noreply.github.com>
2020-07-22 10:26:56 +0530

    Event Bubbling

---
+++ Even Bubbling and Trackling(Capturing)/script.js
@@ -0,0 +1,12 @@
+document.querySelector("#grand")
+    .addEventListener("click", ()=>{
+        console.log('Grand Clicked')
+    }, true)
+    document.querySelector("#parent")
+    .addEventListener("click", ()=>{
+        console.log('Parent Clicked')
+    }, true)
+    document.querySelector("#child")
+    .addEventListener("click", ()=>{
+        console.log('child Clicked')
+    }, true)
---
+++ XMLHttpRequest/xhr.js
@@ -0,0 +1,13 @@
+const getBtn = document.getElementById('get-btn');
+const postBtn = document.getElementById('post-btn');
+
+const getData = ()=>{
+
+}
+
+const postData = ()=>{
+
+}
+
+getBtn.addEventListener('click', getData);
+postBtn.addEventListener('click', postData);
